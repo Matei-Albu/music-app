ca6883da589c6e3726175c36429407a65b0ad9bc
Matei-Albu <albumat0529@gmail.com>
2025-04-16 13:23:29 -0400

    added user authentication and login feature

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "musicappb1d1e5a4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/pages/app-page.js
+++ src/pages/app-page.js
@@ -1,5 +1,17 @@
+import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+
 const AppPage = () => {
-    return <h1>This is the login page</h1>;
+    return (
+      <Authenticator>
+        {({signOut }) => (
+          <dif>
+            <h1>Music app content</h1>
+            <button onClick={signOut}>Sign Out</button>
+          </dif>
+        )}
+      </Authenticator>
+    );
   };
   
   export default AppPage;
